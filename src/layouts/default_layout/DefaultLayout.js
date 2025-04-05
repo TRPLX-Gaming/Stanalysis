05536445e91ec9f21522690b15e051e5fc071e44
TRPLX_Gaming <icgabriel2007@gmail.com>
2025-04-05 10:31:39 +0100

    Update DefaultLayout.js

--- src/layouts/default_layout/DefaultLayout.js
+++ src/layouts/default_layout/DefaultLayout.js
@@ -1,7 +1,10 @@
 import Layout from './../Layout.js'
+import {getComponent} from './../../ComponentModuleManager.js';
 
 export default class DefaultLayout extends Layout {
   constructor(app,title) {
     super(app,title)
+    this.components = []
+    this.content = document.createDocumentFragment()
   }
 }
